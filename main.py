e1681d3e65e90312aa77bf0d06a35e1c3ddd5136
polt.gun@gmail.com <pierrenicolas.doumer@laposte.net>
2020-12-29 18:11:28 +0100

    imports and init

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from colors import BACKGROUND
 from kivy.app import App
 from kivy.properties import ObjectProperty
 import boardGUI
+import analysisWidgets
+import chess
 from kivy.base import Builder
 Builder.load_file("kv/app.kv")
 
@@ -16,6 +18,10 @@ class WindowLayout(BoxLayout):
 class MyChessApp(App):
     def build(self):
         return WindowLayout()
+
+    def on_start(self):
+        self.root.boardGUI.board = chess.Board()
+        pass
 
 
 if __name__ == '__main__':
