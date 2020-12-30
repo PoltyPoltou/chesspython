2929bfbc44144e5bd179edd43e7e0d0256e4aca3
Simon PIERRE <simon.pierre@eurodecision.com>
2020-12-30 12:51:23 +0100

    fixed engine path for linux and windows

--- analysis.py
+++ analysis.py
@@ -2,6 +2,7 @@ import threading
 import chess
 import chess.engine
 import time
+import os
 
 
 class BoardAnalysisWrapper():
@@ -15,12 +16,14 @@ class BoardAnalysisWrapper():
             self.stopFlag = False
             self.threads = 1
             self.setDaemon(True)
+            self.finished = False
             print("Eval thread Launched")
 
         def run(self):
             while(not self.wrapper.board.is_game_over() and not self.stopFlag):
                 boardCopy = self.wrapper.board.copy()
                 with self.wrapper.engine.analysis(boardCopy, self.limit, options={"threads": self.threads}) as analysis:
+                    self.finished = False
                     for info in analysis:
                         if(info.get('score') != None):
                             self.infoMove = info
@@ -30,12 +33,18 @@ class BoardAnalysisWrapper():
                             break
                     # on dors si l'analyse est finie (limit atteinte)
                     while(not self.stopFlag and self.wrapper.board.fen() == boardCopy.fen()):
+                        self.finished = True
                         time.sleep(0.1)
             self.wrapper.engine.quit()
             print("Eval thread Killed")
         pass
 
-    def __init__(self, board, engineStr="./engines/stockfish12.exe"):
+    def __init__(self, board, engineStr="DEFAULT"):
+        if engineStr == "DEFAULT":
+            if os.name.startswith('posix'):
+                engineStr = "./engines/stockfish_20090216_x64_bmi2"
+            else:
+                engineStr = "./engines/stockfish12.exe"
         self.board: chess.Board = board
         self.engine: chess.engine.SimpleEngine = chess.engine.SimpleEngine.popen_uci(
             engineStr)
@@ -65,12 +74,13 @@ class BoardAnalysisWrapper():
     def stopThread(self):
         self.evalThread.stopFlag = True
 
+    def hasFinished(self):
+        return self.evalThread.finished
+
 
 class ThreadGameAnalysis(threading.Thread):
-    def __init__(self, game: chess.Board, engineStr="./engines/stockfish12.exe", pv=3, threads=3, limit=chess.engine.Limit(depth=18)):
+    def __init__(self, game: chess.Board, pv=3, threads=3, limit=chess.engine.Limit(depth=18)):
         super().__init__(self)
-        self.engine: chess.engine.SimpleEngine = chess.engine.SimpleEngine.popen_uci(
-            engineStr)
         self.engine.configure({"threads": threads})
         self.threads = threads
         self.limit = limit
@@ -82,10 +92,13 @@ class ThreadGameAnalysis(threading.Thread):
 
     def run(self):
         self.time = time.time()
+        wrapper = BoardAnalysisWrapper(self.root)
+        wrapper.start()
         for move in self.moveStack:
             self.root.push(move)
-            self.analyseList.append(self.engine.analyse(
-                self.root, self.limit, multipv=self.pv))
+            while(not wrapper.hasFinished()):
+                time.sleep(0.2)
+            self.analyseList.append(wrapper.getEngineAnalysis())
             self.progress.set(100 * self.root.ply() / len(self.moveStack))
         self.time = time.time() - self.time
         self.engine.quit()
