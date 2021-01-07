b20ee4d553a64c82b1395ce6a4390a92bd8cca13
polt.gun@gmail.com <pierrenicolas.doumer@laposte.net>
2021-01-07 16:21:35 +0100

    double click to load pgn

--- chesswindow.py
+++ chesswindow.py
@@ -4,6 +4,7 @@ from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.filechooser import FileChooserListView
 from kivy.uix.popup import Popup
 import boardGUI
 import arrow
@@ -12,6 +13,17 @@ import gamecontroller
 import os
 from movelist import MoveList
 from typing import List, Optional
+
+
+class MyFileChooserListView(FileChooserListView):
+    loaderWidget = ObjectProperty(None)
+
+    def on_touch_down(self, touch):
+        x, y = touch.pos
+        if(self.collide_point(x, y) and touch.is_double_tap):
+            self.loaderWidget.load(self.path, self.selection)
+        else:
+            super().on_touch_down(touch)
 
 
 class LoadDialog(FloatLayout):
