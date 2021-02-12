9066240a659a3498fabfc3820eb395b00caaad32
NP-Polty <polt.gun@gmail.com>
2021-02-12 02:15:57 +0000

    profiling inside a folder init for module import

--- profiling/stats.py
+++ profiling/stats.py
@@ -1,9 +1,14 @@
 import pstats
 import cProfile
+import os
+import sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
 from main import MyChessApp
-cProfile.run(statement='MyChessApp().run()', filename="stats.txt")
-f = open("output.txt", mode="w")
-a: pstats.Stats = pstats.Stats("stats.txt", stream=f)
+cProfile.run(statement='MyChessApp().run()', filename="./profiling/stats.txt")
+f = open("./profiling/output.txt", mode="w")
+a: pstats.Stats = pstats.Stats("./profiling/stats.txt", stream=f)
 a.sort_stats(pstats.SortKey.TIME)
 a.reverse_order()
 a.print_stats("movelist.py")
