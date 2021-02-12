875a5e25888759c4d9eca1fa4ac5d38aba8d06a1
NP-Polty <polt.gun@gmail.com>
2021-02-12 02:20:14 +0000

    faster dropbox and highlight loaded game

--- dropbox.py
+++ dropbox.py
@@ -1,18 +1,32 @@
+from typing import Set
+from chess.pgn import GameNode
+from kivy.uix.behaviors.button import ButtonBehavior
 from kivy.uix.dropdown import DropDown
 from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.base import Builder
+from kivy.properties import BooleanProperty
+Builder.load_file("./kv/dropbox.kv")
 
 
-class GameButton(Button):
+class GameButton(ButtonBehavior, Label):
+    selected = BooleanProperty(False)
+    instances = set()
 
     def __init__(self, game, controller, dropdown, **kwargs):
+        self.selected = False
         super().__init__(**kwargs)
         self.game = game
         self.controller = controller
         self.dropdown = dropdown
+        GameButton.instances.add(self)
 
     def loadGame(self, instance):
-        self.controller.loadGame(self.game)
         self.dropdown.dismiss()
+        self.controller.loadGame(self.game)
+        for b in GameButton.instances:
+            b.selected = False
+        self.selected = True
 
 
 class GameMenu(DropDown):
@@ -27,10 +41,13 @@ class GameMenu(DropDown):
                             height=44, size_hint=(1, None))
 
         if "Date" in game.headers and "StartTime" in game.headers and "White" in game.headers and "Black" in game.headers:
-            button.text = game.headers["Date"]+" - " + game.headers["StartTime"] + \
+            button.text = game.headers["Date"] + " - " + game.headers["StartTime"] + \
                 "\n" + game.headers["White"] + " - " + game.headers["Black"]
         else:
             button.text = "Local game - " + str(self.localCount)
             self.localCount += 1
         button.bind(on_release=button.loadGame)
         self.add_widget(button, len(self.children[0].children))
+
+    def get_top_game(self):
+        return self.children[0].children[-1].game.game()
